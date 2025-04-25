4cebde7dae15f869bdae20b77aaa7253c85cee6c
ShadyEgypt <shadyrafat60@gmail.com>
2025-04-25 17:25:55 +0200

    update communicator pkg

---
+++ rescue_communicator/launch/test_triage_to_audioout_launch.py
@@ -0,0 +1,18 @@
+from launch import LaunchDescription
+from launch_ros.actions import Node
+
+def generate_launch_description():
+    return LaunchDescription([
+        Node(
+            package='rescue_search_tasks',
+            executable='search_and_rescue_node',
+            name='search_and_rescue_node',
+            output='screen'
+        ),
+        Node(
+            package='rescue_communicator',
+            executable='communicator_node',
+            name='communicator_node',
+            output='screen'
+        ),
+    ])
--- rescue_communicator/setup.py
+++ rescue_communicator/setup.py
@@ -10,7 +10,7 @@ setup(
         ('share/ament_index/resource_index/packages',
             ['resource/' + package_name]),
         ('share/' + package_name, ['package.xml']),
-        ('share/' + package_name + '/launch', ['launch/communicator_launch.py']),
+        ('share/' + package_name + '/launch', ['launch/communicator_launch.py', 'launch/test_triage_to_audioout_launch.py']),
     ],
     install_requires=['setuptools'],
     zip_safe=True,
