195a65ab5ceb232ae032aa263ff7d2dd008ee9d0
ShadyEgypt <shadyrafat60@gmail.com>
2025-04-27 00:32:42 +0200

    add the last node to be launched

--- rescue_core_system/launch/test_full_system_launch.py
+++ rescue_core_system/launch/test_full_system_launch.py
@@ -4,6 +4,12 @@ from launch_ros.actions import Node
 def generate_launch_description():
     return LaunchDescription([
         Node(
+            package='rescue_structural_analysis',
+            executable='structural_analysis_node',
+            name='structural_analysis_node',
+            output='screen'    
+        ),
+        Node(
             package='rescue_perception',
             executable='perception_node',
             name='perception_node',
