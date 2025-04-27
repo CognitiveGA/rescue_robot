11b0e71300a2c538c2a8207b04ee204a591a1448
Gian Marco Balia <03_fuori_mastoide@icloud.com>
2025-04-27 23:00:36 +0200

    Quick fix to documentation on read the docs (maybe)

--- rescue_structural_analysis/__init__.py
+++ rescue_structural_analysis/__init__.py
@@ -1,3 +1,4 @@
 from .rescue_structural_analysis import obstacle_and_damage_detection, structural_analysis_node, structural_risk_assessment
+from .test import test_obstacle_and_damage_detection, test_structural_risk_assessment
 
 __all__ = ["obstacle_and_damage_detection", "structural_analysis_node", "structural_risk_assessment"]
